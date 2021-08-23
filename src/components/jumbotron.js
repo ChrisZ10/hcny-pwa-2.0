caa95978bab2038a6cfe8a030aa8fe7b9d00e0c5
Chris Zhao <chrislinzhao@gmail.com>
2021-08-23 14:32:47 -0400

    built jumbotron

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import { getImage } from "gatsby-plugin-image";
 import { convertToBgImage } from "gbimage-bridge";
 import BackgroundImage from "gatsby-background-image";
+import { ChevronDownIcon } from '@heroicons/react/outline';
 
 const Jumbotron = () => {
 
@@ -23,6 +24,17 @@ const Jumbotron = () => {
   );
   const image = getImage(placeholderImage);
   const bgImage = convertToBgImage(image);
+
+  const [scrollTop, setScrollTop] = useState(0);
+  const [scrolling, setScrolling] = useState(true);
+
+  useEffect(() => {
+    const onScroll = ( event ) => {
+      setScrollTop(event.target.documentElement.scrollTop);
+      setScrolling(event.target.documentElement.scrollTop < scrollTop);
+    };
+    window.addEventListener("scroll", onScroll);
+  }, [scrollTop]);
   
   return (
     <BackgroundImage
@@ -36,6 +48,12 @@ const Jumbotron = () => {
           <span>紐約豐收</span>
           <span>靈糧堂</span>
         </h1>
+        <div className="jumbotron-btn-container">
+          <a href="#sunday-stream" className="jumbotron-btn">觀看主日直播</a>
+        </div>
+      </div>
+      <div className="jumbotron-chevron-container">
+        <ChevronDownIcon className={scrolling? "jumbotron-chevron-down active" : "jumbotron-chevron-down"}/>
       </div>
     </BackgroundImage>
   );
