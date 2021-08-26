de7b5d84839aad00076bfedde3aba6426a3fcb97
Chris Zhao <chrislinzhao@gmail.com>
2021-08-26 15:26:08 -0400

    built footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,16 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <h1 className="bg-gray-800">Footer</h1>
+    <div className="box bg-gray-800">
+      <p className="p text-gray-300 text-center loose">
+        建造榮耀的教會<br></br>
+        <span className="text-2xl font-normal">紐約豐收靈糧堂</span>
+      </p>
+      <p className="p sm text-gray-300 text-center">
+        54-47 Little Neck Pkwy, Little Neck, NY 11362<br></br>
+        info@hcny.org<span class="mx-2"></span>+1(718)715-1730  
+      </p>
+    </div>
   );
 };
 
