89e1dc7c674affc566861bb127cb01af5447199e
Chris Zhao <chrislinzhao@gmail.com>
2021-08-31 11:57:47 -0400

    added top bar to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,54 @@ import React from "react";
 import Layout from "../components/layout";
 import Head from "../components/head";
 import Jumbotron from "../components/jumbotron";
-import Card from "../components/card";
-import { useConfig } from "../hooks/useMongoDb";
-import Service from "../components/service";
+import { SiYoutube } from "react-icons/si";
+// import { useConfig } from "../hooks/useMongoDb";
 
 const Home = () => {
 
-  const { sermonYoutubeId } = useConfig();
+  // const { sermonYoutubeId } = useConfig();
 
   return (
     <Layout>
       <Head subtitle="首頁"/>
       <Jumbotron />
+      <div className="grid lg:grid-cols-3 bg-primary mx-4 my-16">
+        <div className="px-8 py-4 border-b border-white">
+          <h3 className="text-lg font-light">中文主日崇拜</h3>
+          <p className="text-sm font-light">
+            <span>SUN 10:30AM</span>
+            <span className="block">
+              實體聚會 + 
+              <a href="#chinese">
+                <SiYoutube className="inline ml-2 mr-1 relative bottom-0.5"/>YouTube直播
+              </a>
+            </span>
+          </p>
+        </div>
+        <div className="px-8 py-4 border-b border-white">
+          <h3 className="text-lg font-light">英文主日崇拜</h3>
+          <p className="text-sm font-light">
+            <span>SUN 10:30AM</span>
+            <span className="block">
+              實體聚會 + 
+              <a href="#english">
+                <SiYoutube className="inline ml-2 mr-1 relative bottom-0.5"/>YouTube直播
+              </a>
+            </span>
+          </p>
+        </div>
+        <div className="px-8 py-4">
+          <h3 className="text-lg font-light">兒童主日崇拜</h3>
+          <p className="text-sm font-light">
+            <span>SUN 10:30AM</span>
+            <span className="block">
+              <a href="#zoom">
+                Zoom線上聚會
+              </a>
+            </span>
+          </p>
+        </div>
+      </div>
       {/* <Card 
         mediaType="video"
         mediaData={sermonYoutubeId}
