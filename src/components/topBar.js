dda687f6ef5e883081720058dfe0f779c527d766
Chris Zhao <chrislinzhao@gmail.com>
2021-08-31 12:53:12 -0400

    created stylesheet for component TopBar

--- src/components/topBar.js
+++ src/components/topBar.js
@@ -3,34 +3,34 @@ import { SiYoutube } from "react-icons/si";
 
 const TopBar = () => {
   return (
-    <div className="grid lg:grid-cols-3 bg-primary mx-4 my-16 lg:w-80vw lg:mx-auto">
-        <div className="px-8 py-4 border-b border-white lg:border-b-0 lg:border-r">
-          <h3 className="text-lg font-light">中文主日崇拜</h3>
-          <p className="text-sm font-light">
+    <div className="top-bar-container">
+        <div className="top-bar-item-container underlined">
+          <h3 className="top-bar-item-title">中文主日崇拜</h3>
+          <p className="top-bar-item-subtitle">
             <span>SUN 10:30AM</span>
             <span className="block">
               實體聚會 + 
               <a href="#chinese">
-                <SiYoutube className="inline ml-2 mr-1 relative bottom-0.5"/>YouTube直播
+                <SiYoutube className="top-bar-item-inline-icon"/>YouTube直播
               </a>
             </span>
           </p>
         </div>
-        <div className="px-8 py-4 border-b border-white lg:border-b-0 lg:border-r">
-          <h3 className="text-lg font-light">英文主日崇拜</h3>
-          <p className="text-sm font-light">
+        <div className="top-bar-item-container underlined">
+          <h3 className="top-bar-item-title">英文主日崇拜</h3>
+          <p className="top-bar-item-subtitle">
             <span>SUN 10:30AM</span>
             <span className="block">
               實體聚會 + 
               <a href="#english">
-                <SiYoutube className="inline ml-2 mr-1 relative bottom-0.5"/>YouTube直播
+                <SiYoutube className="top-bar-item-inline-icon"/>YouTube直播
               </a>
             </span>
           </p>
         </div>
-        <div className="px-8 py-4">
-          <h3 className="text-lg font-light">兒童主日崇拜</h3>
-          <p className="text-sm font-light">
+        <div className="top-bar-item-container">
+          <h3 className="top-bar-item-title">兒童主日崇拜</h3>
+          <p className="top-bar-item-subtitle">
             <span>SUN 10:30AM</span>
             <span className="block">
               <a href="#zoom">
