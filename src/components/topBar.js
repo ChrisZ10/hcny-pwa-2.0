c1084fe5fc01859ccd4bd139c032b742082b256d
Chris Zhao <chrislinzhao@gmail.com>
2021-08-31 13:26:53 -0400

    responsive design of component TopBar

--- src/components/topBar.js
+++ src/components/topBar.js
@@ -3,41 +3,43 @@ import { SiYoutube } from "react-icons/si";
 
 const TopBar = () => {
   return (
-    <div className="top-bar-container">
-        <div className="top-bar-item-container underlined">
-          <h3 className="top-bar-item-title">中文主日崇拜</h3>
-          <p className="top-bar-item-subtitle">
-            <span>SUN 10:30AM</span>
-            <span className="block">
-              實體聚會 + 
-              <a href="#chinese">
-                <SiYoutube className="top-bar-item-inline-icon"/>YouTube直播
-              </a>
-            </span>
-          </p>
-        </div>
-        <div className="top-bar-item-container underlined">
-          <h3 className="top-bar-item-title">英文主日崇拜</h3>
-          <p className="top-bar-item-subtitle">
-            <span>SUN 10:30AM</span>
-            <span className="block">
-              實體聚會 + 
-              <a href="#english">
-                <SiYoutube className="top-bar-item-inline-icon"/>YouTube直播
-              </a>
-            </span>
-          </p>
-        </div>
-        <div className="top-bar-item-container">
-          <h3 className="top-bar-item-title">兒童主日崇拜</h3>
-          <p className="top-bar-item-subtitle">
-            <span>SUN 10:30AM</span>
-            <span className="block">
-              <a href="#zoom">
-                Zoom線上聚會
-              </a>
-            </span>
-          </p>
+    <div className="top-bar">
+      <div className="top-bar-container">
+          <div className="top-bar-item-container underlined">
+            <h3 className="top-bar-item-title">中文主日崇拜</h3>
+            <p className="top-bar-item-subtitle">
+              <span>SUN 10:30AM</span>
+              <span className="top-bar-item-span">
+                實體聚會 + 
+                <a href="#chinese">
+                  <SiYoutube className="top-bar-item-inline-icon"/>YouTube直播
+                </a>
+              </span>
+            </p>
+          </div>
+          <div className="top-bar-item-container underlined">
+            <h3 className="top-bar-item-title">英文主日崇拜</h3>
+            <p className="top-bar-item-subtitle">
+              <span>SUN 10:30AM</span>
+              <span className="top-bar-item-span">
+                實體聚會 + 
+                <a href="#english">
+                  <SiYoutube className="top-bar-item-inline-icon"/>YouTube直播
+                </a>
+              </span>
+            </p>
+          </div>
+          <div className="top-bar-item-container">
+            <h3 className="top-bar-item-title">兒童主日崇拜</h3>
+            <p className="top-bar-item-subtitle">
+              <span>SUN 10:30AM</span>
+              <span className="top-bar-item-span">
+                <a href="#zoom">
+                  Zoom線上聚會
+                </a>
+              </span>
+            </p>
+          </div>
         </div>
       </div>
   );
